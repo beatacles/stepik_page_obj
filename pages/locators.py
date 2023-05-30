77f077c3b106ddd417883758f5d1d3ae62d9e7ea
beatacles <iam.dkalugin@gmail.com>
2023-05-30 15:48:13 +0300

    add some code for login page: LoginPage, LoginPageLocators & 2 tests

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,16 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+    LOGIN_EMAIL_ADDRESS = (By.CSS_SELECTOR, "id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR,"id_login-password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "[name='login_submit']")
+
+    REGISTRATION_EMAIL_ADDRESS = (By.CSS_SELECTOR,"id_registration-email")
+    REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "id_registration-password1")
+    REGISTRATION_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "id_registration-password2")
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR,"[name = 'registration_submit']")
